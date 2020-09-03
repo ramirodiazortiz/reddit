c0176d4731d6c53e7d58c25b5c4955eda9197078
Ramiro Diaz <rdiaz@pch.com>
2020-09-03 01:18:41 -0300

    Add footer view (with both indicator and dismiss all button)

---
+++ Reddit Viewer/View/TopPostListFooter.swift
@@ -0,0 +1,29 @@
+//
+//  TopPostListFooter.swift
+//  reddit
+//
+//  Created by Ramiro Diaz on 03/09/2020.
+//  Copyright © 2020 Ramiro Diaz. All rights reserved.
+//
+
+import UIKit
+
+class TopPostListFooter: UIView {
+
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView?
+	@IBOutlet weak var dismissAllButton: UIButton?
+	
+	func setup(target: Any, selector: Selector) {
+		dismissAllButton?.addTarget(target, action: selector, for: .touchUpInside)
+		dismissAllButton?.setTitle(NSLocalizedString("DismissAll", comment: ""), for: .normal)
+	}
+
+	func swithToLoadingState(isLoading: Bool) {
+		activityIndicator?.isHidden = !isLoading
+		dismissAllButton?.isHidden = isLoading
+	}
+
+	func hideDismissButton(hide: Bool) {
+		dismissAllButton?.isHidden = hide
+	}
+}
